4b02fb0d845fdda4cb9f7cc3d853e9cc47ba0a94
bradakan <rocky-tempelaars@live.nl>
2015-05-01 10:59:43 +0100

    cmmit before pull and started on getting index finger positions

---
+++ Assets/TrackFingerMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrackFingerMovement : MonoBehaviour {
+
+
+    private HandModel handModel;
+
+	// Use this for initialization
+	void Start () 
+    {
+        handModel = GetComponent<HandModel>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
