fc95b4e3f93176238c707fc39bd568e4ec3d4b64
bradakan <rocky-tempelaars@live.nl>
2015-05-12 15:12:29 +0100

    commiting before pull buttons work am working on rotaing input

---
+++ Assets/FollowHand.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowHand : MonoBehaviour {
+
+    private Vector3 startPosition;
+    bool mayIFollow = false;
+
+    void Start() 
+    {
+        startPosition = transform.position;
+    }
+
+    public void OntriggerStay(Collider other) 
+    {
+        if (other.tag == "Handpalm")
+        {
+            Vector3 temp = other.transform.position;
+            temp.x = transform.position.x;
+            temp.y = transform.position.y;
+            transform.position = temp;
+        }
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        transform.position = startPosition;
+    }
+
+    public void setMayIFollow(bool input) 
+    {
+        mayIFollow = input;
+    }
+
+    public bool getMayIFollow() 
+    {
+        return mayIFollow;
+    }
+}
---
+++ Assets/RotationScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotationScript : MonoBehaviour 
+{
+	public bool isRotating;
+	private bool rotateRight;
+	private bool rotateLeft;
+	
+	void Start () 
+	{
+		isRotating = false;
+		rotateRight = false;
+		rotateLeft = false;
+	}
+	
+	void Update () 
+	{
+		//ChoiceRotation("right");
+		//ChoiceRotation("left");
+		//RotateRight()
+		//RotateLeft()
+	}
+
+	public void ChoiceRotation(string direction)
+	{
+		if(direction == "left")
+		{
+			isRotating = true;
+			rotateLeft = true;
+			transform.Rotate(Vector3.up * (Time.deltaTime * 5));
+		}else if(direction == "right")
+		{
+			isRotating = true;
+			rotateRight = true;
+			transform.Rotate(Vector3.down * (Time.deltaTime * 5));
+		}else
+		{
+			isRotating = false;
+			rotateLeft = false;
+			rotateRight = false;
+		}
+	}
+
+	/*public void RotateLeft()
+	{
+		transform.Rotate(Vector3.up * (Time.deltaTime * 5));
+	}
+	public void RotateRight()
+	{
+		transform.Rotate(Vector3.down * (Time.deltaTime * 5));
+	}*/
+}
